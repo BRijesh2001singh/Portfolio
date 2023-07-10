04a04a45670d27ccf6c4b650d52a3984f825dfa4
Brijesh <brijesh1205singh@gmail.com>
2023-07-11 00:35:15 +0530

    fourth-commit

--- script.js
+++ script.js
@@ -17,15 +17,14 @@ let typed=new Typed('.auto-input',{
 let navLinks=document.querySelectorAll('nav ul li a');
 //get all sections
 let sections=document.querySelectorAll('section');
-console.log(sections);
 window.addEventListener('scroll',function(){
-  const scrollPos=window.scrollY+20
+  const scrollPos=window.scrollY
   sections.forEach(section=>{
     if(scrollPos>section.offsetTop && scrollPos<(section.offsetTop+section.offsetHeight)){
       navLinks.forEach(link=>{
         link.classList.remove('active');
         if(section.getAttribute('id')===link.getAttribute('href').substring(1)){
-          link.classList.add('active');
+          link.classList.add('active')
         }
       });
     }
