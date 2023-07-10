137a170c5373dcdb66d5cb76db73d63ff0548445
Brijesh <brijesh1205singh@gmail.com>
2023-07-10 18:20:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+//mobile menubar 
+let menubtn=document.getElementById('Menubtn');
+menubtn.addEventListener('click',function(){
+  document.querySelector('body').classList.toggle('mobile-nav-active');
+  this.classList.toggle('fa-xmark');
+})
+//typing effect
+let typed=new Typed('.auto-input',{
+  strings:['Front-End Developer','Android Developer','Back-End Developer','Problem Solver'],
+  typeSpeed:35,
+  backSpeed:30,
+  backDelay:2000,
+  loop:true,
+})
+
+//adding smooth scroll
+let navLinks=document.querySelectorAll('nav ul li a');
+//get all sections
+let sections=document.querySelectorAll('section');
+console.log(sections);
+window.addEventListener('scroll',function(){
+  const scrollPos=window.scrollY+20
+  sections.forEach(section=>{
+    if(scrollPos>section.offsetTop && scrollPos<(section.offsetTop+section.offsetHeight)){
+      navLinks.forEach(link=>{
+        link.classList.remove('active');
+        if(section.getAttribute('id')===link.getAttribute('href').substring(1)){
+          link.classList.add('active');
+        }
+      });
+    }
+  });
+});
