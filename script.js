91cd0cdba2f56e6e91a00ff3ac31315dbfb1fd27
BrijeshSingh <brijesh1205singh@gmail.com>
2024-08-02 23:26:09 +0530

    updates

--- script.js
+++ script.js
@@ -1,33 +1,18 @@
-//mobile menubar 
-let menubtn = document.getElementById('Menubtn');
-const matchwidth = window.matchMedia('(max-width:1050px)');
-menubtn.addEventListener('click', function () {
-  document.querySelector('body').classList.toggle('mobile-nav-active');
-  this.classList.toggle('fa-xmark');
-  if (!matchwidth.matches)
-    document.querySelector('main').classList.toggle('mobile-section-active');
-})
-//typing effect
-let typed = new Typed('.auto-input', {
-  strings: ['Front-End Developer', 'Android Developer', 'Back-End Developer', 'Problem Solver'],
-  typeSpeed: 35,
-  backSpeed: 30,
-  backDelay: 2000,
-  loop: true,
-})
+function loadpage() {
+    const navlinks = document.querySelectorAll('header nav a');
 
-let navLinks = document.querySelectorAll('nav ul li a');
-let sections = document.querySelectorAll('section');
-window.addEventListener('scroll', function () {
-  const scrollPos = window.scrollY
-  sections.forEach(section => {
-    if (scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)) {
-      navLinks.forEach(link => {
-        link.classList.remove('active');
-        if (section.getAttribute('id') === link.getAttribute('href').substring(1)) {
-          link.classList.add('active')
-        }
-      });
-    }
-  });
-});
+    navlinks.forEach(links => {
+        links.addEventListener('click', (event) => {
+            navlinks.forEach(link => link.classList.remove('active'));
+            event.target.classList.add('active');
+        });
+    });
+}
+
+window.onload = function () {
+    setTimeout(function () {
+        document.getElementById('loading').style.display = 'none';
+    }, 2000);
+}
+loadpage();
+
