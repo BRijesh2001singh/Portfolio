30a280e8c5aea1d1e91bf8b0cea7491d74c520fe
BrijeshSingh <brijesh1205singh@gmail.com>
2024-09-12 23:21:43 +0530

    fixes

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function loadpage() {
 window.onload = function () {
     setTimeout(function () {
         document.getElementById('loading').style.display = 'none';
-    }, 2000);
+    }, 0);
 }
 loadpage();
 
