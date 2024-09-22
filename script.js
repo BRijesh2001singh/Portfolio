cd3a4754513b3bcbddf886b7782f63f4f8b92aa2
BrijeshSingh <brijesh1205singh@gmail.com>
2024-09-22 17:29:07 +0530

    updates

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
-function loadpage() {
-    const navlinks = document.querySelectorAll('header nav a');
-
-    navlinks.forEach(links => {
-        links.addEventListener('click', (event) => {
-            navlinks.forEach(link => link.classList.remove('active'));
-            event.target.classList.add('active');
-        });
-    });
-}
-
-window.onload = function () {
+window.onload = async function () {
+    loadingScreen();
     setTimeout(function () {
         document.getElementById('loading').style.display = 'none';
-    }, 2000);
+    }, 3000);
+
 }
-loadpage();
+document.addEventListener("DOMContentLoaded", function () {
+    navBar();
+});
+function loadingScreen() {
+    const loadprocess = document.querySelector(".loader-process");
+    const loadspan = loadprocess.querySelectorAll("span");
+
+    loadprocess.style.visibility = "hidden";
+    let delay = 0;
+    setTimeout(() => {
+        loadspan.forEach(span => {
+            span.style.visibility = "hidden";
+            setTimeout(() => {
+                span.style.visibility = "visible";
+            }, delay)
+            delay += 200;
+        })
+    }, 500)
+}
+
+
+function navBar() {
+    const sections = document.querySelectorAll(".section");
+    const navbar = document.querySelectorAll(".nav-link");
+
+    window.addEventListener('scroll', () => {
+        let currentsection = "";
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (window.scrollY >= sectionTop - 50 && window.scrollY < sectionTop + sectionHeight - 50) {
+                currentsection = section.getAttribute("id");
+            }
+        })
+
+        navbar.forEach(link => {
+            link.classList.remove("active");
+            if (link.getAttribute("href") === `#${currentsection}`) {
+                link.classList.add("active");
+            }
+        })
+    })
+}
+
+
+
 
